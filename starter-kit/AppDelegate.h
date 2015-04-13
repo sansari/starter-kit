3bd1d3da4d31443ceaa51e726e45f47dc3f4b4c8
Salman Ansari <salman.ansari@gmail.com>
2015-04-12 21:25:59 -0700

    Initial Commit

---
+++ starter-kit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  starter-kit
+//
+//  Created by Salman Ansari on 4/12/15.
+//  Copyright (c) 2015 daretodream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
